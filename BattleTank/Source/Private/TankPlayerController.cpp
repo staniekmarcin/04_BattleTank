5b97b4b2d86e887a5a9569936b21f5f670c61475
staniekm <marcin.staniek97@gmail.com>
2020-04-02 14:18:00 +0200

    Log which tank is possesed

--- BattleTank/Source/Private/TankPlayerController.cpp
+++ BattleTank/Source/Private/TankPlayerController.cpp
@@ -3,7 +3,24 @@
 //#include "BattleTank.h"
 #include "TankPlayerController.h"
 
-ATank* ATankPlayerController::GetControlledTank() const
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    auto ControlledTank = GetControlledTank();
+    if(!ControlledTank)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing %s"), *(ControlledTank->GetName()));
+    }
+    
+    UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+}
+
+ATank *ATankPlayerController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());
 }
