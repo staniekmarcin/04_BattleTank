f870712233f8fabb74d81d1ed8f9faeebfb424b9
Marcin Staniek <marcin.staniek97@gmail.com>
2020-04-10 18:58:31 +0200

    Revert "Revert "BlueprinReadOnly""
    This reverts commit 57f607490e09484808b9a1a692869b49db363bf7.

--- BattleTank/Source/Private/TankTrack.cpp
+++ BattleTank/Source/Private/TankTrack.cpp
@@ -4,9 +4,6 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-    //auto Name = GetName();
-    UE_LOG(LogTemp, Warning, TEXT("throttle: %f"), Throttle);
-
     // TODO clamp actual throttle value so player can't over-drive
     auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
     auto ForceLocation = GetComponentLocation();
