fb7a3b97355e458709290b2d6d81593e82283a19
Marcin Staniek <marcin.staniek97@gmail.com>
2020-04-13 17:16:02 +0200

    Enum(erations) for color of the crosshair

--- BattleTank/Source/Public/TankAimingComponent.h
+++ BattleTank/Source/Public/TankAimingComponent.h
@@ -6,6 +6,15 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+// Enum for aiming state
+UENUM()
+enum class EFiringState : uint8
+{
+	Reloading,
+	Aiming,
+	Locked
+};
+
 //forward declaration
 class UTankBarrel;
 class UTankTurret;
@@ -25,13 +34,15 @@ public:
 	void SetTurretReference(UTankTurret *TurretToSet);
 
 public:
-
 	void AimAt(FVector HitLocation, float LaunchSpeed);
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Aiming;
 
 private:
 	UTankBarrel *Barrel = nullptr;
-
 	UTankTurret *Turret = nullptr;
-	
+
 	void MoveBarrelTowards(FVector AimDirection);
 };
